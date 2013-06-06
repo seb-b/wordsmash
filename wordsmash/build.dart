b895427a838df624a7dee019bb28a34809b6f15d
Seb Brown <Sebastian.Brown@equella.com>
2013-06-06 17:12:14 +1000

    rename and index

--- wordsmash/build.dart
+++ wordsmash/build.dart
@@ -2,5 +2,5 @@ import 'package:web_ui/component_build.dart';
 import 'dart:io';
 
 void main(){
-  build(new Options().arguments,['web/wordsmash.html']);
+  build(new Options().arguments,['web/sentence.html', 'web/index.html']);
 }
