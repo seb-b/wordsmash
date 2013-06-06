e16c5609c29f6b20da4923645c772591ce726edd
Seb Brown <Sebastian.Brown@equella.com>
2013-06-06 10:21:45 +1000

    Getting random word

--- wordsmash/web/wordsmash.dart
+++ wordsmash/web/wordsmash.dart
@@ -1,4 +1,8 @@
 import 'dart:html';
+import 'package:web_ui/web_ui.dart';
+
+@observable
+String word = "help";
 
 void main() {
   query("#sample_text_id")
@@ -14,4 +18,15 @@ void reverseText(MouseEvent event) {
   }
   query("#sample_text_id").text = buffer.toString();
 }
-//comment
+
+void getWord()
+{
+  word = "halp";
+  String url = "http://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=false&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=9426b5f9c67e03853f5410a188e06bc4136900201e3fd92eb";
+  var request = HttpRequest.getString(url).then(response);
+}
+
+void response(String responseText)
+{
+  print(responseText);
+}
