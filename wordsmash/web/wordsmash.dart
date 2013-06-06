5ec4061279dc05df9b96f8cfb38d2d87de0747f7
Seb Brown <Sebastian.Brown@equella.com>
2013-06-06 10:26:21 +1000

    example code begone

--- wordsmash/web/wordsmash.dart
+++ wordsmash/web/wordsmash.dart
@@ -1,32 +1,22 @@
 import 'dart:html';
 import 'package:web_ui/web_ui.dart';
+import 'dart:json';
 
 @observable
 String word = "help";
 
 void main() {
-  query("#sample_text_id")
-    ..text = "Click me!"
-    ..onClick.listen(reverseText);
-}
 
-void reverseText(MouseEvent event) {
-  var text = query("#sample_text_id").text;
-  var buffer = new StringBuffer();
-  for (int i = text.length - 1; i >= 0; i--) {
-    buffer.write(text[i]);
-  }
-  query("#sample_text_id").text = buffer.toString();
 }
 
 void getWord()
 {
-  word = "halp";
   String url = "http://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=false&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=9426b5f9c67e03853f5410a188e06bc4136900201e3fd92eb";
   var request = HttpRequest.getString(url).then(response);
 }
 
-void response(String responseText)
+void response(String response)
 {
-  print(responseText);
+  Map data = parse(response);
+  word = data["word"];
 }
